a0e4603673476050b8c3b384631ab59c7ae95937
Matt Brooks <mbrooks@digium.com>
2021-10-25 09:44:16 -0500

    Reauthenticate on 401 errors
    When the client recieves a 401 error, we need to immediate try to
    reauthenticate.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const nodeFetch = require("node-fetch");
 const { GraphQLClient, gql } = require("graphql-request");
-const polly = require("polly-js");
 
 const defaultOptions = {
+  debug: false,
   retry: 3,
-  delay: 1000,
+  retryDelay: 1000,
 };
 
 // Define network connection errors
@@ -23,6 +23,9 @@ const getGqlEndpoint = (baseUrl) => {
   return `${baseUrl}/admin/api/api/gql`;
 };
 
+// Utility function to make application wait the specified milliseconds
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 class FreepbxGqlClient {
   #oAuthClientId;
   #oAuthClientSecret;
@@ -38,50 +41,57 @@ class FreepbxGqlClient {
     this.#oAuthEndpoint = getOAuthEndpoint(baseUrl);
     this.#gqlEndpoint = getGqlEndpoint(baseUrl);
     this.#options = { ...defaultOptions, ...options };
-
-    polly.defaults.delay = this.#options.delay;
   }
 
-  async handleRetry(fn) {
-    return polly()
-      .logger((err) => {
-        console.error("Caught GraphQL Error, Retrying:", err);
-      })
-      .handle((err) => {
-        if (err.code && connectionErrors.includes(err.code)) {
-          return true;
-        }
-
-        if (err.message === "Client request timeout") {
-          return true;
-        }
-
-        return false;
-      })
-      .waitAndRetry(this.#options.retry)
-      .executeForPromise(() => {
-        return fn;
-      });
+  logError(message, err) {
+    if (this.#options.debug) {
+      console.error(message, err);
+    }
   }
 
-  async getOAuthAccessToken() {
-    if (this.#gqlAccessToken) {
-      return this.#gqlAccessToken;
+  retryNetworkError(err) {
+    if (err.code && connectionErrors.includes(err.code)) {
+      return true;
     }
 
+    if (err.message === "Client request timeout") {
+      return true;
+    }
+
+    return false;
+  }
+
+  async authenticateRequestWithRetry(retries, retryDelay) {
     const oAuthParams = new URLSearchParams();
     oAuthParams.append("grant_type", "client_credentials");
     oAuthParams.append("client_id", this.#oAuthClientId);
     oAuthParams.append("client_secret", this.#oAuthClientSecret);
 
-    const authResult = await nodeFetch(this.#oAuthEndpoint, {
+    return nodeFetch(this.#oAuthEndpoint, {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       },
       body: oAuthParams,
-    });
+    }).catch(async (err) => {
+      // retry request on network error
+      if (retries > 0 && this.retryNetworkError(err)) {
+        this.logError("Caught GraphQL Error, Retrying:", err);
+        await delay(retryDelay);
+        return this.authenticateRequestWithRetry(retries - 1, retryDelay * 2);
+      }
 
+      throw err;
+    });
+  }
+
+  async authenticate() {
+    const retries = this.#options.retry;
+    const retryDelay = this.#options.retryDelay;
+    const authResult = await this.authenticateRequestWithRetry(
+      retries,
+      retryDelay
+    );
     if (authResult.status !== 200) {
       throw new Error("Authentication Failed");
     }
@@ -96,19 +106,41 @@ class FreepbxGqlClient {
       return;
     }
 
-    const accessToken = await this.handleRetry(this.getOAuthAccessToken());
-    const requestHeaders = {
-      headers: {
-        Authorization: "Bearer " + accessToken,
-      },
-    };
-    const gqlClient = new GraphQLClient(this.#gqlEndpoint, requestHeaders);
+    if (!this.#gqlAccessToken) {
+      await this.authenticate();
+    }
+
+    const gqlClient = new GraphQLClient(this.#gqlEndpoint);
+    gqlClient.setHeader("Authorization", "Bearer " + this.#gqlAccessToken);
     this.#gqlClient = gqlClient;
   }
 
-  async request(...args) {
+  async requestWithRetry(retries, retryDelay, ...args) {
     await this.buildClient();
-    return this.handleRetry(this.#gqlClient.request(...args));
+    return this.#gqlClient.request(...args).catch(async (err) => {
+      // retry request on network error
+      if (retries > 0 && this.retryNetworkError(err)) {
+        this.logError("Caught GraphQL Error, Retrying:", err);
+        await delay(retryDelay);
+        return this.requestWithRetry(retries - 1, retryDelay * 2, ...args);
+      }
+
+      // If access denied, try to reauthenticate immediately. Do not add any delay
+      if (retries > 0 && err.response && err.response.status === 401) {
+        this.logError("Caught GraphQL Error, Retrying:", err);
+        this.#gqlAccessToken = null;
+        this.#gqlClient = null;
+        return this.requestWithRetry(retries - 1, retryDelay, ...args);
+      }
+
+      throw err;
+    });
+  }
+
+  async request(...args) {
+    const retries = this.#options.retry;
+    const retryDelay = this.#options.retryDelay;
+    return this.requestWithRetry(retries, retryDelay, ...args);
   }
 }
 
